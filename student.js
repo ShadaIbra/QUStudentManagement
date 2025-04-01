d0f3175a132051dcd39c767a6f9762fe06577818
ShadaIbra <shada.ibrahim@yahoo.com>
2025-04-01 23:20:20 +0300

    search in student-main working

--- student.js
+++ student.js
@@ -1,57 +1,81 @@
 document.addEventListener("DOMContentLoaded", async function () {
 
-    localStorage.getItem("loggedInUser");
+    const user = JSON.parse(localStorage.getItem("loggedInUser"));
+    let courses = [];
 
-    async function renderCourses() {
+    function renderCourse(course) {
+        const tableRow = document.createElement("tr");
 
+        const courseName = document.createElement("td");
+        courseName.innerText = course.courseName;
+        tableRow.appendChild(courseName);
+
+        const courseCategory = document.createElement("td");
+        courseCategory.innerText = course.category;
+        tableRow.appendChild(courseCategory);
+
+        const courseCRN = document.createElement("td");
+        courseCRN.innerText = course.crn;
+        tableRow.appendChild(courseCRN);
+
+        const courseInstructor = document.createElement("td");
+        courseInstructor.innerText = course.instructor;
+        tableRow.appendChild(courseInstructor);
+
+        const courseStatus = document.createElement("td");
+        courseStatus.innerText = `${course.totalSeats - course.takenSeats} of ${course.totalSeats} seats remaining`;
+        tableRow.appendChild(courseStatus);
+
+        const buttonCol = document.createElement("td");
+        tableRow.appendChild(buttonCol);
+
+        const registerButton = document.createElement("button");
+        registerButton.innerText = "Register";
+        buttonCol.appendChild(registerButton);
+
+        registerButton.addEventListener("click", function () {
+            if (registerButton.classList.contains("registered")) {
+                registerButton.classList.remove("registered");
+                registerButton.innerText = "Register";
+            } else {
+                registerButton.classList.add("registered");
+                registerButton.innerText = "Registered";
+            }
+        });
+
+        return tableRow;
+
+    }
+
+    function renderCourses(courses) {
+        const tableBody = document.querySelector("tbody");
+        tableBody.replaceChildren();
+
+        courses.forEach(course => {
+            tableBody.appendChild(renderCourse(course));
+        });
+    }
+
+    async function loadCourses() {
         const res = await fetch('repo/data/courses.json');
         const courses = await res.json();
 
-        console.log(courses);
-
-        const availableCourses = courses.filter(course => course.available);
-
-        console.log(availableCourses);
-
-        const tableBody = document.querySelector("tbody");
-
-        for (const course of availableCourses) {
-
-            const tableRow = document.createElement("tr");
-            tableBody.appendChild(tableRow);
-
-            const courseName = document.createElement("td");
-            courseName.innerText = course.courseName;
-            tableRow.appendChild(courseName);
-
-            const courseCategory = document.createElement("td");
-            courseCategory.innerText = course.category;
-            tableRow.appendChild(courseCategory);
-
-            const courseCRN = document.createElement("td");
-            courseCRN.innerText = course.crn;
-            tableRow.appendChild(courseCRN);
-
-            const courseInstructor = document.createElement("td");
-            courseInstructor.innerText = course.instructor;
-            tableRow.appendChild(courseInstructor);
-
-            const courseStatus = document.createElement("td");
-            courseStatus.innerText = `${course.totalSeats - course.takenSeats} of ${course.totalSeats} seats remaining`;
-            tableRow.appendChild(courseStatus);
-
-            const buttonCol = document.createElement("td");
-            tableRow.appendChild(buttonCol);
-
-            const registerButton = document.createElement("button");
-            registerButton.innerText = "Register";
-            buttonCol.appendChild(registerButton);
-
-        }
+        return courses.filter(course => course.available);
     }
 
-    renderCourses();
+    function searchCourses() {
+        const search = document.querySelector("#name-search").value.toLowerCase().trim();
 
+        const filteredCourses = courses.filter(course =>
+            course.courseName.toLowerCase().includes(search) ||
+            course.category.toLowerCase().includes(search));
 
+        renderCourses(filteredCourses);
+    }
+
+    document.querySelector("#search-btn").addEventListener("click", searchCourses);
+
+    courses = await loadCourses();
+    renderCourses(courses);
 
 });
