09da2416fadb1aa352040f343c448e21e49f63b1
ShadaIbra <shada.ibrahim@yahoo.com>
2025-04-02 00:58:40 +0300

    register button works

--- student.js
+++ student.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", async function () {
 
     const user = JSON.parse(localStorage.getItem("loggedInUser"));
+
     let courses = [];
+    let student;
 
     function renderCourse(course) {
         const tableRow = document.createElement("tr");
@@ -33,18 +35,54 @@ document.addEventListener("DOMContentLoaded", async function () {
         registerButton.innerText = "Register";
         buttonCol.appendChild(registerButton);
 
-        registerButton.addEventListener("click", function () {
+        if (student.pendingCRN.includes(course.crn)){
+            registerButton.classList.add("registered");
+            registerButton.innerText = "Registered";
+        };
+
+        registerButton.addEventListener("click", event => handleRegister(event, course.crn));
+
+
+        return tableRow;
+    }
+
+    function handleRegister(event, courseCRN) {
+
+        const passingGrades = ["A", "B+", "B", "C+", "C", "D"];
+
+        const passedCourses = student.completedCourses.filter(course => passingGrades.includes(course.grade)).map(course => course.crn);
+
+        const course = getCourse(courseCRN);
+
+        const hasPassedPrereqs =
+            course.prereqsCRN.length === 0 ||
+            course.prereqsCRN.every(crn => passedCourses.includes(crn));
+
+        if (hasPassedPrereqs) {
+            const registerButton = event.target;
+
             if (registerButton.classList.contains("registered")) {
                 registerButton.classList.remove("registered");
                 registerButton.innerText = "Register";
+
+                //crud remove
+
+                const index = student.pendingCRN.indexOf(courseCRN);
+                if (index !== -1) {
+                    student.pendingCRN.splice(index, 1);
+                }
+
             } else {
                 registerButton.classList.add("registered");
                 registerButton.innerText = "Registered";
+
+                //crud add
+
+                if (!student.pendingCRN.includes(courseCRN)) {
+                    student.pendingCRN.push(courseCRN);
+                }
             }
-        });
-
-        return tableRow;
-
+        }
     }
 
     function renderCourses(courses) {
@@ -63,6 +101,17 @@ document.addEventListener("DOMContentLoaded", async function () {
         return courses.filter(course => course.available);
     }
 
+    function getCourse(crn) {
+        return courses.find(course => course.crn === crn);
+    }
+
+    async function getStudent() {
+        const res = await fetch('repo/data/students.json');
+        const students = await res.json();
+
+        return students.find(student => student.email === user.email);
+    }
+
     function searchCourses() {
         const search = document.querySelector("#name-search").value.toLowerCase().trim();
 
@@ -75,7 +124,11 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     document.querySelector("#search-btn").addEventListener("click", searchCourses);
 
+    student = await getStudent();
+
     courses = await loadCourses();
     renderCourses(courses);
+
+
 
 });
