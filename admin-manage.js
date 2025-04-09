2f95a03a52d71b0ec5f8f1010a50bf56e0393594
ShadaIbra <shada.ibrahim@yahoo.com>
2025-04-09 04:33:52 +0300

    adding courses and classes working

--- admin-manage.js
+++ admin-manage.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", async function () {
-
     categories = [];
     courses = [];
-    classes = [];
     instructors = [];
 
     function saveCourses() {
         localStorage.setItem("courses", JSON.stringify(courses));
-    }
-
-    function saveClasses() {
-        localStorage.setItem("classes", JSON.stringify(classes));
     }
 
     async function loadCourses() {
@@ -39,19 +33,6 @@ document.addEventListener("DOMContentLoaded", async function () {
         return data;
     }
 
-    async function loadClasses() {
-        const saved = localStorage.getItem("classes");
-        if (saved) {
-            return JSON.parse(saved);
-        }
-
-        const res = await fetch("data/classes.json");
-        let data = await res.json();
-
-        localStorage.setItem("classes", JSON.stringify(data));
-        return data;
-    }
-
     async function loadInstructors() {
         const saved = localStorage.getItem("instructors");
         if (saved) {
@@ -64,6 +45,9 @@ document.addEventListener("DOMContentLoaded", async function () {
         localStorage.setItem("instructors", JSON.stringify(data));
         return data;
     }
+    function getCourse(code) {
+        return courses.find(course => course.code === code);
+    }
 
     function renderPage() {
         const categoriesSelect = document.querySelector("#category");
@@ -71,11 +55,11 @@ document.addEventListener("DOMContentLoaded", async function () {
         categoriesSelect.innerHTML += categories.map(category => `<option value="${category}">${category}</option>`).join('');
 
         const prereqsSelect = document.querySelector("#prereqs");
-        prereqsSelect.innerHTML = courses.map(course => `<option value="${course.crn}">${course.courseName} (${course.crn})</option>`).join('');
+        prereqsSelect.innerHTML = courses.map(course => `<option value="${course.code}">${course.courseName} (${course.code})</option>`).join('');
 
-        const courseSelect = document.querySelector("#class-crn");
+        const courseSelect = document.querySelector("#course-code");
         courseSelect.innerHTML = `<option value="" disabled selected>Select a Course</option>`;
-        courseSelect.innerHTML += courses.map(course => `<option value="${course.crn}">${course.courseName} (${course.crn})</option>`).join('');
+        courseSelect.innerHTML += courses.map(course => `<option value="${course.code}">${course.courseName} (${course.code})</option>`).join('');
 
         const instructorSelect = document.querySelector("#instructor");
         instructorSelect.innerHTML = `<option value="" disabled selected>Select a Instructor</option>`;
@@ -90,24 +74,25 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         const courseName = formData.get("course-name").trim();
         const category = formData.get("category");
-        const crn = formData.get("crn").trim();
+        const code = formData.get("code").trim();
 
         const prereqs = formData.getAll("prereqs");
 
         const enablePref = form.querySelector("#enable-pref").checked;
 
-        if (courses.find(course => course.crn === crn)) {
-            alert("A course with this CRN already exists. Please choose a unique CRN.");
+        if (courses.find(course => course.code === code)) {
+            alert("A course with this Code already exists. Please choose a unique Code.");
             return;
         }
 
         const newCourse = {
             courseName,
             category,
-            crn,
-            prereqsCRN: prereqs || [],
+            code,
             preferenceOpen: enablePref,
+            prereqscodeCode: prereqs || [],
             preferenceList: [],
+            classes: []
         }
 
         courses.push(newCourse);
@@ -123,21 +108,21 @@ document.addEventListener("DOMContentLoaded", async function () {
         const form = event.target;
         const formData = new FormData(form);
 
-        const classCode = formData.get("class-code").trim();
-        const crn = formData.get("class-crn");
+        const crn = formData.get("class-crn").trim();
+        const code = formData.get("course-code");
         const totalSeats = parseInt(formData.get("total-seats"));
 
-        // const course = courses.find(course => course.crn === crn);
-
         const instructor = formData.get("instructor");
+        const course = getCourse(code);
 
-        if (classes.find(c => c.classCode === classCode)) {
-            alert("A class with this code already exists.");
-            return;
+        for (const c of courses) {
+            if (c.classes.find(c => c.crn === crn)) {
+                alert("A class with this crn already exists.");
+                return;
+            }
         }
 
         const newClass = {
-            classCode,
             crn,
             instructor,
             takenSeats: 0,
@@ -146,21 +131,19 @@ document.addEventListener("DOMContentLoaded", async function () {
             validated: false
         }
 
-        classes.push(newClass);
+        course.classes.push(newClass);
         form.reset();
 
-        saveClasses();
+        saveCourses();
         renderPage();
     }
 
     courses = await loadCourses();
     categories = await loadCategories();
-    classes = await loadClasses();
     instructors = await loadInstructors();
 
     console.log(categories);
     console.log(courses);
-    console.log(classes);
     console.log(instructors);
 
     document.querySelector("#add-course-form").addEventListener("submit", addCourse);
@@ -170,6 +153,5 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     // localStorage.removeItem("courses");
     // localStorage.removeItem("categories");
-    // localStorage.removeItem("classes");
     // localStorage.removeItem("instructors");
 });
